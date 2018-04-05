d43f3b42b5a05cd7616150f308a5c3204da3146b
Clairehere <b_claire@hotmail.com>
2018-04-05 17:18:48 +0200

    preparation api

--- app/src/main/java/fr/wildcodeschool/winners/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/winners/MainActivity.java
@@ -2,12 +2,64 @@ package fr.wildcodeschool.winners;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static String API_KEY = "remplace cette valeur par ta clé API";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //String url = "https://api.openweathermap.org/data/2.5/weather?q=Toulouse&appid=" + API_KEY;
+        // Crée une file d'attente pour les requêtes vers l'API
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // TODO : URL de la requête vers l'API
+        String url = "";
+
+        // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // TODO : traiter la réponse
+                        try {
+                            JSONArray weather = response.getJSONArray("weather");
+                            for (int i = 0; i < weather.length(); i++) {
+                                JSONObject weatherInfos = (JSONObject) weather.get(i);
+                                String description = weatherInfos.getString("description");
+                                Toast.makeText(MainActivity.this, description, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Afficher l'erreur
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+
+        // On ajoute la requête à la file d'attente
+        requestQueue.add(jsonObjectRequest);
     }
 }
