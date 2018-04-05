c87fb1e9415a71f98a53872cffa6d8506e560b49
Clairehere <b_claire@hotmail.com>
2018-04-05 19:58:43 +0200

    Api Marche!

--- app/src/main/java/fr/wildcodeschool/winners/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/winners/MainActivity.java
@@ -3,18 +3,23 @@ package fr.wildcodeschool.winners;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private final static String API_KEY = "remplace cette valeur par ta clé API";
@@ -27,27 +32,54 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         // TODO : URL de la requête vers l'API
-        String url = "";
+        String url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json";
+
+
+
+
 
         // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
                 Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
+                new Response.Listener<JSONArray>() {
 
                     @Override
-                    public void onResponse(JSONObject response) {
-                        // TODO : traiter la réponse
+                    public void onResponse(JSONArray response) {
                         try {
-                            JSONArray weather = response.getJSONArray("weather");
-                            for (int i = 0; i < weather.length(); i++) {
-                                JSONObject weatherInfos = (JSONObject) weather.get(i);
-                                String description = weatherInfos.getString("description");
-                                Toast.makeText(MainActivity.this, description, Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
+
+                                                       TextView tvname = findViewById(R.id.textView_name);
+                            TextView tvattaque = findViewById(R.id.textView_str);
+                            TextView tvSpd = findViewById(R.id.textView_spd);
+                            TextView tvvie = findViewById(R.id.textView_vie);
+                            ImageView profil =findViewById(R.id.imageView_superheros);
+
+                            JSONObject name10= (JSONObject) response.get(10);
+                            String names10 = name10.getString("name");
+                            tvname.setText(names10);
+
+
+                            JSONObject stat = name10.getJSONObject("powerstats");
+                            int attaque =stat.getInt("strength");
+                            tvattaque.setText(String.valueOf(attaque));
+
+                            int speed =stat.getInt("speed");
+                            tvSpd.setText(String.valueOf(speed));
+
+                            int vie = stat.getInt("durability");
+                            tvvie.setText(String.valueOf(vie));
+
+                            JSONObject img = name10.getJSONObject("images");
+                            String imgprofil =img.getString("sm");
+                            Glide.with(MainActivity.this).load(imgprofil).into(profil);
+
+
+
+                        } catch (JSONException e){
                             e.printStackTrace();
                         }
                     }
+
+
                 },
                 new Response.ErrorListener() {
 
@@ -60,6 +92,6 @@ public class MainActivity extends AppCompatActivity {
         );
 
         // On ajoute la requête à la file d'attente
-        requestQueue.add(jsonObjectRequest);
+        requestQueue.add(jsonArrayRequest);
     }
 }
